52d6e9b9410506c79eba8da969f4682cc6f23408
rok-piculin <rok.piculin@gmail.com>
2025-01-14 09:04:03 +0100

    test

---
+++ Prva/Prva/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Prvi poskus");
+        }
+    }
+}
